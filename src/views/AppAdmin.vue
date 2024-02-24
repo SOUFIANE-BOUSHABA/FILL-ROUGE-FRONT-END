1482b308ae298a19fd7ca2f249f50c47cb3208fb
souf139 <soufianboushaba12@gmail.com>
2024-02-24 12:13:59 +0100

    this commit is for creating the code for make sam effect in page admin

--- src/views/AppAdmin.vue
+++ src/views/AppAdmin.vue
@@ -31,12 +31,12 @@
             <header class="">
             <div class="w-full navv d-flex shadow justify-content-between align-items-center">
 
-                <button class="btn btn-light" id="leftBtn" onclick="toggleAside()">
+                <button class="btn btn-light" id="leftBtn"  @click="toggleAside()">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z"/>
                     </svg>
                 </button>
-                <button class="btn btn-light" id="rightBtn" onclick="toggleAside()">
+                <button class="btn btn-light" id="rightBtn"  @click="toggleAside()">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"/>
                     </svg>
@@ -63,14 +63,37 @@
   </template>
   
   <script>
-  
-  
   export default {
-    components: {
+    methods: {
+      toggleAside() {
+        var aside = document.getElementById("myAside");
+        var righttt = document.getElementById("right");
+        var rightttBtn = document.getElementById("rightBtn");
+        var leftBtn = document.getElementById("leftBtn");
+        var links = document.querySelectorAll(".link");
+  
+        if (aside.style.width === "5%") {
+          aside.style.width = "17%";
+          righttt.style.width = "83%";
+          leftBtn.style.display = "block";
+          rightttBtn.style.display = "none";
+          links.forEach(function (link) {
+            link.style.display = "block";
+          });
+        } else {
+          aside.style.width = "5%";
+          righttt.style.width = "95%";
+          leftBtn.style.display = "none";
+          rightttBtn.style.display = "block";
+  
+          links.forEach(function (link) {
+            link.style.display = "none";
+          });
+        }
+      },
     },
   };
   </script>
-  
   <style scoped>
   
 body {
